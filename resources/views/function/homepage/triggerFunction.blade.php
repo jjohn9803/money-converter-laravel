134b174c72026b67b422063c87e68a5a454f406b
jjohn9803 <jjohn9803@gmail.com>
2022-08-12 17:30:14 +0800

    t

--- resources/views/function/homepage/triggerFunction.blade.php
+++ resources/views/function/homepage/triggerFunction.blade.php
@@ -235,13 +235,14 @@
         //
         var bank_accounts_support = false;
         $bank_accounts.forEach(element => {
+            console.log(element['country_id']);
+            console.log(country_id);
             if (element['country_id'].indexOf(country_id) > -1) {
                 bank_accounts_support = true;
             }
         });
 
         if ($bank_accounts.length < 1 || !bank_accounts_support) {
-
             var curr = $('#sel_from_curr option:selected').text();
             Swal.fire(
                 "{!! __('content.swal.sorry') !!}",
