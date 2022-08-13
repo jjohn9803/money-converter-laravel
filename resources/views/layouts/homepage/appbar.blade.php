bd74d47d07d4ea8b7b61edbb3215a024efdb92b0
jjohn9803 <jjohn9803@gmail.com>
2022-08-13 13:40:06 +0800

    r

--- resources/views/layouts/homepage/appbar.blade.php
+++ resources/views/layouts/homepage/appbar.blade.php
@@ -31,7 +31,7 @@
                             <a class="nav-link" href="#footer-section">{{ __('content.appbar.contact') }}</a>
                         </li>
                         @auth
-                            <li class="dropdown cus-nav-user-b">
+                            <div class="dropdown cus-nav-user-b">
                                 <div class="dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown"
                                     aria-haspopup="true"  style="white-space: nowrap;">
                                     <span class="nav-user">{{ auth()->user()->name }}</span>
@@ -51,9 +51,9 @@
                                     </li>
                                     <li class="cus-li2"><a id='logout'>{{ __('content.appbar.logout') }}</a></li>
                                 </div>
-                            </li>
+                            </div>
                         @else
-                            <li class="cus-nav nav-item" id='loginbefore' style="height: 40px;">
+                            <nav class="cus-nav nav-item" id='loginbefore'>
                                 <ul class="cus-ul">
                                     <li class="cus-li">
                                         <a class="cus-a" data-toggle="modal" data-target="#loginModal">
@@ -61,15 +61,15 @@
                                         </a>
                                     </li>
                                 </ul>
-                            </li>
+                            </nav>
                         @endauth
-                        <li class="dropdown cus-nav-lang-b">
+                        <div class="dropdown cus-nav-lang-b">
                             <div class="dropdown-toggle" type="button" id="dropdownMenu3" data-toggle="dropdown"
                                 aria-haspopup="true"  style="white-space: nowrap;">
                                 <span class="nav-user"><i class="fa fa-globe" style="font-size: 24px;"></i></span>
                                 <span class="nav-user2">{{ __('content.appbar.language.title') }}</span>
                             </div>
-                            <ul class="dropdown-menu cus-nav-lang-li" aria-labelledby="dropdownMenu3">
+                            <div class="dropdown-menu cus-nav-lang-li" aria-labelledby="dropdownMenu3">
                                 <li class="cus-li2" id='localeEn'>
                                     <a><span class='fi fi-us mx-2'></span>{{ __('content.appbar.language.en') }}</a>
                                 </li>
@@ -77,7 +77,7 @@
                                     <a><span class='fi fi-cn mx-2'></span>{{ __('content.appbar.language.zh-CN') }}</a>
                                 </li>
                             </div>
-                        </li>
+                        </div>
                     </ul>
                 </div>
                 @auth
